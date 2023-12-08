eb0ea7e08c57ab187ba6d26ef8e0d97b2f44bb98
carla braga <carlamab2808@outlook.com>
2023-12-07 21:23:48 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
-//efeito do botão voltar ao Topo
+//Efeito do botão voltar ao Topo
+function topo() {
+    window.scrollTo(
+        {
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        }
+    )
+}
+
 
 //Validação de Login
+function login() {
+    var logado = 0;
+    var usuario = document.getElementsById("usuario").value;
+    var senha = document.getElementsById("senha").value;
+
+    if(usuario == 'admin' && senha == '3535') {
+        window.location = "index.html";
+        logado = 1;
+    }
+
+    if(logado == 0) {
+        alert("Acesso Negado. Dados Incorretos");
+    }
+}
 
 //Ativar alert no botão cadastrar
